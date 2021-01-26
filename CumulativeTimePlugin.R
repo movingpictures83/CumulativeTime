71ae24830c241cf3ee74f8839a992f72b95809d7
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-01-26 05:38:41 -0500

    Made flexible

--- CumulativeTimePlugin.R
+++ CumulativeTimePlugin.R
@@ -1,3 +1,7 @@
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+
 require(graphics)
 library("caret")
 library("spls")
@@ -5,40 +9,50 @@ library(mlbench)
 input <- function(inputfile) {
   parameters <<- read.table(inputfile, as.is=T);
   rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <- paste(pfix, "/", sep="")
+  }
   print("READING INPUT FILES...");
-  t1 <<- read.table(toString(parameters["training",2]), sep = "\t", header =FALSE, stringsAsFactors=FALSE)#, nrow=20000)
-  t2 <<- read.table(toString(parameters["clinical",2]), sep="\t", header = TRUE,  stringsAsFactors=FALSE)
+  target <<- toString(parameters["target", 2])
+  t1 <<- read.table(paste(pfix, toString(parameters["training",2]), sep=""), sep = "\t", header =FALSE, stringsAsFactors=FALSE)#, nrow=20000)
+  t2 <<- read.table(paste(pfix, toString(parameters["clinical",2]), sep=""), sep="\t", header = TRUE,  stringsAsFactors=FALSE)
+  t3 <<- read.table(paste(pfix, toString(parameters["times",2]), sep=""))
+  joinby <<- toString(parameters["joinby", 2])
+  orderby <<- toString(parameters["orderby", 2])
+  id <<- toString(parameters["id", 2])
+  myX <<- toString(parameters["x", 2])
+  classcol <<- toString(parameters["classcol", 2])
   print("DONE");
 }
 
 run <- function() {
-   t2[t2$STUDYID == "DEE4X H1N1",]$STUDYID <<- "H1N1"
-   t2[t2$STUDYID == "DEE3 H1N1",]$STUDYID <<- "H1N1"
-   t2[t2$STUDYID == "DEE2 H3N2",]$STUDYID <<- "H3N2"
-   t2[t2$STUDYID == "DEE5 H3N2",]$STUDYID <<- "H3N2"
-   t2[t2$STUDYID == "Rhinovirus Duke",]$STUDYID <<- "Rhinovirus"
-   t2[t2$STUDYID == "Rhinovirus UVA",]$STUDYID <<- "Rhinovirus"
-
    t1 <<- as.data.frame(t(t1), stringsAsFactors=FALSE)
-   colnames(t1)[1] = "CEL"
-   x <<- merge(t1, t2, by ="CEL", stringsAsFactors=FALSE)
 
-   v1 <<- subset(x, x$STUDYID == "DEE1 RSV")
-   v1 <<- v1[order(v1$SUBJECTID),]
+colnames(t1)[1] = joinby
+   x <<- merge(t1, t2, by =joinby, stringsAsFactors=FALSE)
+
+   v1 <<- subset(x, as.character(unlist(x[id])) == target)
+   #v1 <<- v1[order(v1$SUBJECTID),]
+   v1 <<- v1[order(as.character(unlist(v1[orderby]))),]
 
    #subjectsV1 <<- intersect(subset(v1$SUBJECTID, v1$TIMEHOURS==0),intersect(subset(v1$SUBJECTID, v1$TIMEHOURS==-24), intersect(subset(v1$SUBJECTID, v1$TIMEHOURS==5), intersect(subset(v1$SUBJECTID, v1$TIMEHOURS==12), intersect(subset(v1$SUBJECTID, v1$TIMEHOURS==36), intersect(subset(v1$SUBJECTID, v1$TIMEHOURS==21.5), subset(v1$SUBJECTID, v1$TIMEHOURS==45.5)))))))
-   timesV1 <<- c(-24, 0, 5, 12, 21.5, 36, 45.5)
+   #timesV1 <<- c(-24, 0, 5, 12, 21.5, 36, 45.5)
+   timesV1 <<- as.numeric(unlist(t3))
+   train_set_size <<- ncol(t1)
+   class_index <<- grep(classcol, colnames(t2))
 }
 
 output <- function(outputfile) {
-t.y = as.factor(v1[v1[,"TIMEHOURS"]==-24,22286])
+#t.y = as.factor(v1[v1[,"TIMEHOURS"]==-24,22286])
+t.y = as.factor(v1[v1[,myX]==timesV1[1],(train_set_size+class_index)])
 # datX = matrix(nrow = length(subjectsV1), ncol = 22277)
 #Create empty dataframe
 for(t in timesV1){
-  t.x <- data.matrix(v1[v1[,"TIMEHOURS"]==t,2:22278])
+  print(t)
+  t.x <- data.matrix(v1[v1[,myX]==t,2:train_set_size])
   # datX <- cbind(datX, t.x)
   #Join t.x to frame
-
   my_pls1 = plsda(t.x, t.y)
 
   res = order(varImp(my_pls1), decreasing = TRUE)[1:1000]
